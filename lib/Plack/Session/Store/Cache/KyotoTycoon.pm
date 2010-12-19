bf4e7b12d696e0de57bdc9d40eadbe3b6ab6670a
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-19 23:45:20 +0900

    initial commit

---
+++ lib/Plack/Session/Store/Cache/KyotoTycoon.pm
@@ -0,0 +1,32 @@
+package Plack::Session::Store::Cache::KyotoTycoon;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Session::Store::Cache::KyotoTycoon -
+
+=head1 SYNOPSIS
+
+  use Plack::Session::Store::Cache::KyotoTycoon;
+
+=head1 DESCRIPTION
+
+Plack::Session::Store::Cache::KyotoTycoon is
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji {at} senchan.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
