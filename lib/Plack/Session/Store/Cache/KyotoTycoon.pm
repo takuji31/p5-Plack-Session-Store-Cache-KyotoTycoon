cd4695fcd8161395feaeaad37d6b94b0eedd5223
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-20 00:15:02 +0900

    initial commit

--- lib/Plack/Session/Store/Cache/KyotoTycoon.pm
+++ lib/Plack/Session/Store/Cache/KyotoTycoon.pm
@@ -1,14 +1,38 @@
 package Plack::Session::Store::Cache::KyotoTycoon;
 use strict;
 use warnings;
+use Cache::KyotoTycoon;
+use JSON::XS;
+
 our $VERSION = '0.01';
+
+sub new {
+    my $class = shift;
+    my $kt    = Cache::KyotoTycoon->new(%_);
+    bless { kt => $kt }, $class;
+}
+
+sub fetch {
+    my ( $self, $session_id ) = @_;
+    decode_json( $self->{kt}->get($session_id) || '{}' );
+}
+
+sub store {
+    my ( $self, $session_id, $session ) = @_;
+    $self->{kt}->set( $session_id, encode_json($session) );
+}
+
+sub remove {
+    my ( $self, $session_id ) = @_;
+    $self->{kt}->remove($session_id);
+}
 
 1;
 __END__
 
 =head1 NAME
 
-Plack::Session::Store::Cache::KyotoTycoon -
+Plack::Session::Store::Cache::KyotoTycoon - Session store for Plack::Session::Store which uses Cache::KyotoTycoon
 
 =head1 SYNOPSIS
 
@@ -16,13 +40,15 @@ Plack::Session::Store::Cache::KyotoTycoon -
 
 =head1 DESCRIPTION
 
-Plack::Session::Store::Cache::KyotoTycoon is
+Plack::Session::Store::Cache::KyotoTycoon is Session store for Plack::Session::Store which uses Cache::KyotoTycoon
 
 =head1 AUTHOR
 
 Nishibayashi Takuji E<lt>takuji {at} senchan.jpE<gt>
 
 =head1 SEE ALSO
+
+Plack::Middleware::Session Cache::KyotoTycoon
 
 =head1 LICENSE
 
